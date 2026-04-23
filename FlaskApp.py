d7e60e978fbf703e94a53e88793709353809ff91
TheScarletBadger <barry.crouch@outlook.com>
2026-04-23 23:41:29 +0100

    Added parsing using instructor library

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, request, redirect
+from pydantic import BaseModel
+import instructor
 import uuid
 import os
 import pdfplumber
 import json
 
 app = Flask(__name__)
+
+#LLM client
+client = instructor.from_provider("ollama/qwen3:4b")
+
+# Define response model
+class report(BaseModel):
+    report_title: str
+    report_date: str
+    wind_today: str
 
 def pdfextract(file):
     outdict = {}
@@ -13,7 +24,15 @@ def pdfextract(file):
             outdict[f'Page {idx} text'] = page.extract_text()
             outdict[f'Table {idx}'] = page.extract_tables()
     json_string = json.dumps(outdict, indent=2)
-    return(json_string)
+    # Extract it from natural language
+    llmoutput = client.chat.completions.create(
+        response_model=report,
+        messages=[{"role": "user", "content": json_string}])
+    
+    return(llmoutput.model_dump())
+
+
+
 
 @app.route('/')  
 def main():  
@@ -43,8 +62,12 @@ def apiupload():
         return redirect('/upload')
     else:
         newname = str(uuid.uuid4())
-        f.save('uploads/' + newname + '.pdf') 
-        return(pdfextract('uploads/' + newname + '.pdf'))
+        f.save('uploads/' + newname + '.pdf')
+        jstr = pdfextract('uploads/' + newname + '.pdf')
+        
+        
+        
+        return(jstr)
 
 
 
--- test.py
+++ test.py
@@ -5,15 +5,40 @@ Created on Wed Apr 22 22:21:14 2026
 @author: Barry
 """
 
+from flask import Flask, render_template, request, redirect
+from pydantic import BaseModel
+import instructor
+import uuid
+import os
 import pdfplumber
 import json
+import jsonify
+app = Flask(__name__)
+
+#LLM client
+client = instructor.from_provider("ollama/qwen3:4b")
+
+# Define response model
+class report(BaseModel):
+    title: str
+    date: str
+    wind: str
 
 outdict = {}
 
-with pdfplumber.open("uploads/75b7e169-947b-4525-821c-b8c424b2c23a.pdf") as pdf:
+with pdfplumber.open("uploads/eh-mwi-wm14455_2026-04-18_160011_4353.pdf") as pdf:
     for idx, page in enumerate(pdf.pages, start=1):
         outdict[f'Page {idx} text'] = page.extract_text()
         outdict[f'Table {idx}'] = page.extract_tables()
 
+
+
 json_string = json.dumps(outdict, indent=2)
-print(json_string)
+
+# Extract it from natural language
+llmoutput = client.chat.completions.create(
+    response_model=report,
+    messages=[{"role": "user", "content": json_string}],
+)
+
+print(llmoutput.model_dump())
